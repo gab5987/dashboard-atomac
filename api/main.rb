20a8a68e0b6fe8562abc2df3d839b048780e86dd
gab5987 <biell.gb4@gmail.com>
2022-10-16 00:21:08 -0300

    Feat: Add api itens

--- api/main.rb
+++
@@ -1,3 +0,0 @@
-require 'sinatra'
-
-# Build the entire thing...
---
+++ api/src/test/java/com/setup/dashboardapp/DashboardAutomacApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.setup.dashboardapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashboardAutomacApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
