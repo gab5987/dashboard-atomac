efa35beefffac3957bf9ac57266ea649e4ccee54
gab5987 <biell.gb4@gmail.com>
2022-10-18 10:46:45 -0300

    Feat: Error page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import './App.scss';
 import Sidebar from './components/Sidebar';
 import Dashboard from './components/Home';
 
+import { Unauthorized } from './RaiseExeption';
+
 const resumeLanguage = require('./data/res_primary_language.json');
 const sharedData = require('./data/shared_data.json');
 
@@ -20,7 +22,7 @@ class App extends React.Component<{},
   componentDidMount() {
     const url = new URLSearchParams(window.location.search);
     this.setState({
-      actualPage: url.get('window_location'), 
+      actualPage: url.get('window_location') || "dashboard", 
       userId: url.get('user_id') || "guest"
     });
   }
@@ -34,6 +36,16 @@ class App extends React.Component<{},
               resumeLanguage={resumeLanguage.itens.dashboard}
               sharedData={sharedData} 
             /> }
+          { 
+            this.state.actualPage === 'monthlyRegister' && ( 
+              this.state.userId === 'guest' ? <Unauthorized /> : <h1> teste </h1>
+            )
+          }
+          { 
+            this.state.actualPage === 'settings' && ( 
+              this.state.userId === 'guest' ? <Unauthorized /> : <h1> teste </h1>
+            )
+          } 
         </div>
       </div>
     );
