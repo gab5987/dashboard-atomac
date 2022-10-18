3f437d6d3035da5c9e3b2e8ed4c3594a0ec68ea5
gab5987 <biell.gb4@gmail.com>
2022-10-18 10:20:06 -0300

    Feat: Getting current user and window location

--- src/App.tsx
+++ src/App.tsx
@@ -7,26 +7,33 @@ import Dashboard from './components/Home';
 const resumeLanguage = require('./data/res_primary_language.json');
 const sharedData = require('./data/shared_data.json');
 
-class App extends React.Component<{}, {actualPage: string}> {
+class App extends React.Component<{},
+  { actualPage: string | null, userId: string | undefined }> {
   constructor(props: any) {
     super(props);
     this.state = {
-      actualPage: 'dashboard',
+      actualPage: null,
+      userId: undefined,
     };
+  }
+
+  componentDidMount() {
+    const url = new URLSearchParams(window.location.search);
+    this.setState({
+      actualPage: url.get('window_location'), 
+      userId: url.get('user_id') || "guest"
+    });
   }
 
   render() {
     return (
       <div className="App">
-        <Sidebar />
+        <Sidebar userId={ this.state.userId } />
         <div className="App-context">
-          {
-            this.state.actualPage === 'dashboard' && <Dashboard 
+          { this.state.actualPage === 'dashboard' && <Dashboard 
               resumeLanguage={resumeLanguage.itens.dashboard}
               sharedData={sharedData} 
-            />
-            
-          }
+            /> }
         </div>
       </div>
     );
