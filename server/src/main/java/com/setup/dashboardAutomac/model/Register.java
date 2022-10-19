2ef2bdac3db6e651d5e5e81a527242be6076e450
gab5987 <biell.gb4@gmail.com>
2022-10-18 21:56:04 -0300

    Feat: Created all variables for Register

--- server/src/main/java/com/setup/dashboardAutomac/model/Register.java
+++ server/src/main/java/com/setup/dashboardAutomac/model/Register.java
@@ -10,13 +10,8 @@ import javax.persistence.Id;
 public class Register {
 
     @Id @GeneratedValue(strategy = GenerationType.AUTO) private Long id;
-
-    @Column(nullable = false) private String name;
-
     public Long getId() { return id;  }
     public void setId(Long id) { this.id = id; }
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
 
     @Override public boolean equals(Object o) {
         if (this == o) return true;
@@ -26,4 +21,28 @@ public class Register {
 
         return id != null ? id.equals(register.id) : register.id == null;
     }
+
+    @Column(nullable = false) private String floor;
+    public String getFloor() { return floor; }
+    public void setFloor(String floor) { this.floor = floor; }
+
+    @Column(nullable = false) private String core;
+    public String getCore() { return core; }
+    public void setCore(String core) { this.core = core; }
+
+    @Column private String name;
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    @Column(nullable = false) private String requestType;
+    public String getRequestType() { return requestType; }
+    public void setRequestType(String requestType) { this.requestType = requestType; }
+
+    @Column(nullable = false) private String date;
+    public String getDate() { return date; }
+    public void setDate(String date) { this.date = date; }
+
+    @Column private String attendant;
+    public String getAttendant() { return attendant; }
+    public void setAttendant(String attendant) { this.attendant = attendant; }
 }
