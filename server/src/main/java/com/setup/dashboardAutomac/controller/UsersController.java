513547eec435f2abcb0c6e9172d3bca19fc1ba07
gab5987 <biell.gb4@gmail.com>
2022-10-19 15:45:19 -0300

    Fix: Better user control

--- server/src/main/java/com/setup/dashboardAutomac/controller/UsersController.java
+++ server/src/main/java/com/setup/dashboardAutomac/controller/UsersController.java
@@ -3,12 +3,7 @@ package com.setup.dashboardAutomac.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
 import com.setup.dashboardAutomac.model.Users;
 import com.setup.dashboardAutomac.repository.UsersRepository;
@@ -18,7 +13,7 @@ import com.setup.dashboardAutomac.repository.UsersRepository;
 public class UsersController {
 
     @Autowired private UsersRepository usersRepository;
-    
+
     @GetMapping public List<Users> getAlUsers() {
         return usersRepository.findAll();
     }
--- server/src/main/java/com/setup/dashboardAutomac/model/Users.java
+++ server/src/main/java/com/setup/dashboardAutomac/model/Users.java
@@ -1,32 +1,28 @@
 package com.setup.dashboardAutomac.model;
 
 import java.io.Serializable;
-import org.hibernate.annotations.GenericGenerator;
+import java.util.UUID;
 
 import javax.persistence.*;
 
 @Entity
-public class Users {
+public class Users implements Serializable {
 
-    @Id
-    @GeneratedValue(generator = "uuid")
-    @GenericGenerator(name = "uuid", strategy = "uuid")
-    @Column private String id;
+    public static String generateString() {
+        String uuid = UUID.randomUUID().toString();
+        return uuid;
+    }
 
+    @Column private String id = generateString();
     public String getId() { return id;  }
     public void setId(String id) { this.id = id; }
 
     @Column private String name;
-    
     public String getName() { return name;  }
     public void setName(String name) { this.name = name; }
 
-    @Override public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Users user = (Users) o;
-
-        return id != null ? id.equals(user.id) : user.id == null;
-    }
+    @Id
+    @Column private String passWord;
+    public String getPassWord() { return passWord; }
+    public void setPassWord(String passWord) { this.passWord = passWord; }
 }
