5be97908b79c82cb0aa7888d3aa616a51db64a6e
gab5987 <biell.gb4@gmail.com>
2022-10-20 15:50:27 -0300

    Feat: Added login page

--- src/App.tsx
+++ src/App.tsx
@@ -8,29 +8,69 @@ import { Unauthorized } from './RaiseExeption';
 
 const resumeLanguage = require('./data/res_primary_language.json');
 const sharedData = require('./data/shared_data.json');
-
 class App extends React.Component<{},
-  { actualPage: string | null, userId: string | undefined }> {
+  { actualPage: string | null, userId: string | undefined, isLoggedIn: boolean }> {
   constructor(props: any) {
     super(props);
     this.state = {
       actualPage: null,
       userId: undefined,
+      isLoggedIn: false
     };
+    this.raiseApp = this.raiseApp.bind(this);
+    this.raiseLogin = this.raiseLogin.bind(this);
   }
 
   componentDidMount() {
     const url = new URLSearchParams(window.location.search);
     this.setState({
       actualPage: url.get('window_location') || "dashboard", 
-      userId: url.get('user_id') || "guest"
+      userId: url.get('user_id') || "guest",
+      isLoggedIn: ( url.get('isLogged') === 'true' && true ) || false,
     });
   }
 
   render() {
     return (
+      <>
+        { this.state.isLoggedIn ? this.raiseApp() : this.raiseLogin() }
+      </>
+    );
+  }
+
+  raiseLogin() {
+    return(
+      <div className='App App-Login'>
+        <div className='App-Login_modal'>
+          <div className='App-Login_modal-context'>
+            <div className='modal-header'>
+              <img width="50%" height="50%" 
+                src="https://seeklogo.com/images/R/republica-federativa-do-brasil-brasao-logo-66C22A3895-seeklogo.com.png"
+              />
+              <h2 className='pt-2'>Sistema de Gestão Predial</h2>
+            </div>
+
+            <form className='pt-3'>
+              <label> insira seu Token: </label>
+              <input className="form-control me-2" type="password"/>
+              <div className='pt-2' />
+
+              <div className='pt-3 d-flex justify-content-center'>
+                <button className="btn btn-primary" type="submit" style={{ marginRight: '10px' }}> Entrar </button>
+                <button className='btn btn-outline-info' style={{ marginRight: '10px' }}> Cadastrar Usuário </button>
+                <button className='btn btn-outline-warning' onClick={() => {this.setState({isLoggedIn: true })}}> Visitante </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+  raiseApp() {
+    return(
       <div className="App">
-        <Sidebar userId={ this.state.userId } />
+        <Sidebar userId={ this.state.userId } isLoggedIn={ this.state.isLoggedIn } />
         <div className="App-context">
           { this.state.actualPage === 'dashboard' && <Dashboard 
               resumeLanguage={resumeLanguage.itens.dashboard}
@@ -48,7 +88,7 @@ class App extends React.Component<{},
           } 
         </div>
       </div>
-    );
+    )
   }
 }
 export default App;
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -12,7 +12,7 @@ import {
     faUser
 } from '@fortawesome/free-solid-svg-icons';
 
-class Sidebar extends React.Component<{ userId: string | undefined }, { }> {
+class Sidebar extends React.Component<{ userId: string | undefined, isLoggedIn: boolean }, { }> {
     render() {
         return(
             <ProSidebar className='App-sidebar border border-dark rounded'>
@@ -29,27 +29,27 @@ class Sidebar extends React.Component<{ userId: string | undefined }, { }> {
                 <SidebarContent>
                     <Menu iconShape="circle">
                         <MenuItem icon={<FontAwesomeIcon icon={faTableColumns} />}>
-                            <a href = {`\?user_id=${this.props.userId}&window_location=dashboard`} > Dashboard </a>
+                            <a href = {`\?user_id=${this.props.userId}&isLogged=${this.props.isLoggedIn.toString()}&window_location=dashboard`} > Dashboard </a>
                         </MenuItem>
     
                         <MenuItem icon={<FontAwesomeIcon icon={faCircleExclamation} />}>
-                            <a href = {`\?user_id=${this.props.userId}&window_location=prevent`} > Preventivas </a>
+                            <a href = {`\?user_id=${this.props.userId}&isLogged=${this.props.isLoggedIn.toString()}&window_location=prevent`} > Preventivas </a>
                         </MenuItem>
     
                         <MenuItem icon={<FontAwesomeIcon icon={faCalendarDays} />}>
-                            <a href = {`\?user_id=${this.props.userId}&window_location=nextEvents`} > Eventos </a>
+                            <a href = {`\?user_id=${this.props.userId}&isLogged=${this.props.isLoggedIn.toString()}&window_location=nextEvents`} > Eventos </a>
                         </MenuItem>
     
                         <MenuItem icon={<FontAwesomeIcon icon={faNewspaper} />}>
-                            <a href = {`\?user_id=${this.props.userId}&window_location=monthlyRegister`} > Relatório mensal </a>
+                            <a href = {`\?user_id=${this.props.userId}&isLogged=${this.props.isLoggedIn.toString()}&window_location=monthlyRegister`} > Relatório mensal </a>
                         </MenuItem>
     
                         <MenuItem icon={<FontAwesomeIcon icon={faGears} />}>
-                            <a href = {`\?user_id=${this.props.userId}&window_location=settings`} > Preferências </a>
+                            <a href = {`\?user_id=${this.props.userId}&isLogged=${this.props.isLoggedIn.toString()}&window_location=settings`} > Preferências </a>
                         </MenuItem>
     
                         <MenuItem icon={<FontAwesomeIcon icon={faQuestion} />}>
-                            <a href = {`\?user_id=${this.props.userId}&window_location=help`} > Ajuda </a>
+                            <a href = {`\?user_id=${this.props.userId}&isLogged=${this.props.isLoggedIn.toString()}&window_location=help`} > Ajuda </a>
                         </MenuItem>
                     </Menu>
                 </SidebarContent>
@@ -57,7 +57,7 @@ class Sidebar extends React.Component<{ userId: string | undefined }, { }> {
                 <SidebarFooter>
                     <Menu iconShape="circle" className='exit-button'>
                         <MenuItem icon={<FontAwesomeIcon icon={faUser} />}>
-                            <a href="/home"> Sair </a>
+                            <a href="/"> Sair </a>
                         </MenuItem>
                     </Menu>
                 </SidebarFooter>
