566dd94fccdb015115abf572821ba2935eca3d88
gab5987 <biell.gb4@gmail.com>
2022-10-24 09:03:04 -0300

    Feat: Add button event listener

--- server/src/main/java/com/setup/dashboardAutomac/RenderGui.java
+++ server/src/main/java/com/setup/dashboardAutomac/RenderGui.java
@@ -1,9 +1,19 @@
 package com.setup.dashboardAutomac;
 
 import java.awt.*;
-import java.awt.event.*;
 import javax.swing.*;
-import javax.swing.event.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.net.URL;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 public class RenderGui extends JPanel {
     private JButton jcomp1;
@@ -13,9 +23,32 @@ public class RenderGui extends JPanel {
     private JList jcomp5;
     private JLabel jcomp6;
     private JLabel jcomp7;
+    private JLabel jcomp8;
 
-    public RenderGui() {
-        //construct preComponents
+    public static void getServerAdderes() {
+        InetAddress ip;
+        String hostname;
+        try {
+            ip = InetAddress.getLocalHost();
+            hostname = ip.getHostName();
+            System.out.println("Your current IP address : " + ip);
+            System.out.println("Your current Hostname : " + hostname);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void openWebpage(String urlString) {
+        try {
+            Desktop.getDesktop().browse(new URL(urlString).toURI());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public RenderGui() throws Exception {
+        BufferedImage img = ImageIO.read(new URL("https://github.com/gab5987/dashboard-atomac/raw/main/server/src/main/java/com/setup/dashboardAutomac/assets/brasao_republica.png"));
+        ImageIcon icon = new ImageIcon(img);
         JMenu arquivoMenu = new JMenu ("Arquivo");
         JMenuItem gerenciar_credenciaisItem = new JMenuItem ("Gerenciar Credenciais");
         arquivoMenu.add (gerenciar_credenciaisItem);
@@ -37,9 +70,16 @@ public class RenderGui extends JPanel {
         jcomp2.add (ajudaMenu);
         jcomp3 = new JLabel ("SISTEMA DA GERENCIAMENTO PREDIAL - JFSC");
         jcomp4 = new JButton ("Documentação da API");
+        jcomp4.addActionListener(new ActionListener() { 
+            public void actionPerformed(ActionEvent e) { 
+              openWebpage("http://localhost:1234");
+            } 
+          } );
+
         jcomp5 = new JList (jcomp5Items);
         jcomp6 = new JLabel ("Servidor ativo !");
-        jcomp7 = new JLabel ("Para o sistema, utilize o link: ");
+        jcomp7 = new JLabel ("Para utilizar o sistema, utilize o ip deste compuatdor. ");     
+        jcomp8 = new JLabel (icon);
 
         //adjust size and set layout
         setPreferredSize (new Dimension (681, 406));
@@ -53,6 +93,7 @@ public class RenderGui extends JPanel {
         add (jcomp5);
         add (jcomp6);
         add (jcomp7);
+        add (jcomp8);
 
         //set component bounds (only needed by Absolute Positioning)
         jcomp1.setBounds (15, 360, 100, 25);
@@ -62,11 +103,18 @@ public class RenderGui extends JPanel {
         jcomp5.setBounds (100, 135, 485, 135);
         jcomp6.setBounds (100, 90, 100, 25);
         jcomp7.setBounds (100, 105, 205, 25);
+        jcomp8.setBounds (510, 30, 100, 100);
     }
     public static void RenderAndShowGui() {
         JFrame frame = new JFrame ("Sistema de Gerenciamento Predial - JFSC");
         frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().add (new RenderGui());
+
+        try {
+            frame.getContentPane().add (new RenderGui());
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
         frame.pack();
         frame.setVisible (true);
     }
